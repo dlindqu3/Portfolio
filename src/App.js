c949935dba1b887b00efb1ccafc0503b472147d1
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-03-18 20:34:14 -0400

    updated for error

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
         <Header sharedData={this.state.sharedData.basic_info} />
         <Switch>
           <Route exact path="/">
-            <Home style="background-color: #99ccff"
+            <Home 
               resumeData={this.state.resumeData}
               sharedData={this.state.sharedData}
             />
